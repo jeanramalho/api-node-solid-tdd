e0d1a9bf225c7ab48fd2a5aae655f909fd3f8ff4
jeanramalho <jeanramalho.dev@gmail.com>
2022-08-23 13:16:06 -0300

    inicia criação da class correction

--- src/core/domain/Entity.ts
+++ src/core/domain/Entity.ts
@@ -1,5 +1,11 @@
-export abstract class Entity {
-    constructor() {
-        
+import crypto from 'crypto'
+
+export abstract class Entity<T> {
+    protected _id: string;
+    public props: T;
+
+    constructor(props: T, id?: string) {
+        this.props = props;
+        this._id = id ?? crypto.randomUUID()
     }
 }
