969bbb1585f23bbac37e4931c8a29e8a4e3091e4
jeanramalho <jeanramalho.dev@gmail.com>
2022-08-23 13:31:59 -0300

    Finaliza class correction

--- src/domain/entities/correction.ts
+++ src/domain/entities/correction.ts
@@ -1,3 +1,21 @@
-class Correction {
-    
+import { createExportDeclaration } from "typescript";
+import { Entity } from "../../core/domain/Entity";
+
+type CorrectionProps = {
+    grade: number;
+    submissionId: string;
+    createdAt: Date;
+
+}
+
+class Correction extends Entity<CorrectionProps> {
+    private constructor (props: CorrectionProps, id?: string) {
+        super(props, id)
+    }
+
+    public create(props: CorrectionProps, id?: string) {
+        const correction = new Correction(props, id)
+
+        return correction
+    }
 }
