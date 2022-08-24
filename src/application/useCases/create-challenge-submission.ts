80a29d8dd1d53321d245b5d38a05ac401d6dc33e
jeanramalho <jeanramalho.dev@gmail.com>
2022-08-24 12:59:03 -0300

    Cria repositorio e verificação de challenges

--- src/application/useCases/create-challenge-submission.ts
+++ src/application/useCases/create-challenge-submission.ts
@@ -1,5 +1,6 @@
 import { Submission } from "../../domain/entities/submission";
 import { StudentsRepository } from "../repositories/StudentsRepository";
+import { ChallengesRepository } from "../repositories/ChallengesRepository"
 
 type CreateChallengeSubmissionRequest = {
     studentId: string;
@@ -8,10 +9,22 @@ type CreateChallengeSubmissionRequest = {
 
 export class CreateChallengeSubmission {
     constructor(
-        private studentsRepositpry: StudentsRepository
+        private studentsRepositpry: StudentsRepository,
+        private challengesRepositpry: ChallengesRepository
     ) {}
 
     async execute({studentId, challengeId} : CreateChallengeSubmissionRequest) {
+        const student = await this.studentsRepositpry.findById(studentId)
+        
+        if(!student) {
+            throw new Error('Student does not exists')
+        }
+        const challenge = await this.challengesRepositpry.findById(challengeId)
+
+        if(!challenge) {
+            throw new Error('Challenge does not exists')
+        }
+        
         const submission = Submission.create({
             studentId,
             challengeId,
