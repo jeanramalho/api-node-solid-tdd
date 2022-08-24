9f4c6b4748fa7e757d5e3a048dc6e8b920e35de1
jeanramalho <jeanramalho.dev@gmail.com>
2022-08-24 13:15:44 -0300

    Implementa inMemory

--- src/application/useCases/create-challenge-submission.spec.ts
+++ src/application/useCases/create-challenge-submission.spec.ts
@@ -1,7 +1,9 @@
 import { CreateChallengeSubmission } from "./create-challenge-submission"
+import { InMemoryStudentsRepository } from "../../../tests/repositories/in-memory-students-repository"
 
 describe('Create challenge submission use case', () => {
     it('shold be able to create a new challenge submission', async () => {
+        const studentsRepository = new InMemoryStudentsRepository()
         const sut = new CreateChallengeSubmission()
 
         const response = await sut.execute({
